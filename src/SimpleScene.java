67281548962afef5f1dbaef65907b4e0c81c9504
raxo <oskar.klang@gmail.com>
2014-01-21 11:00:47 +0100

    test done working in 1500

---
+++ src/SimpleScene.java
@@ -0,0 +1,28 @@
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.media.opengl.*;
+import javax.media.opengl.awt.GLCanvas;
+
+public class SimpleScene {
+    public static void main(String[] args) {
+        GLProfile glp = GLProfile.getDefault();
+        GLCapabilities caps = new GLCapabilities(glp);
+        GLCanvas canvas = new GLCanvas(caps);
+
+        Frame frame = new Frame("AWT Window Test");
+        frame.setSize(300, 300);
+        frame.add(canvas);
+        frame.setVisible(true);
+        
+        // by default, an AWT Frame doesn't do anything when you click
+        // the close button; this bit of code will terminate the program when
+        // the window is asked to close
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
