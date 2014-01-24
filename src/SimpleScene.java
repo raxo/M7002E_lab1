2b65f9637715677069812f43a8df1a3475dd397f
raxo <oskar.klang@gmail.com>
2014-01-24 15:31:32 +0100

    more complex and completed star

--- src/SimpleScene.java
+++ src/SimpleScene.java
@@ -2,11 +2,13 @@
 import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.nio.ByteBuffer;
 import java.util.Random;
 import java.util.Vector;
 
 import javax.media.opengl.*;
 import javax.media.opengl.awt.GLCanvas;
+import javax.media.opengl.awt.GLJPanel;
 
 import com.jogamp.opengl.util.*;
 
@@ -20,7 +22,7 @@ public class SimpleScene implements GLEventListener {
         GLCanvas canvas = new GLCanvas(caps);
 
         Frame frame = new Frame("AWT Window Test");
-        frame.setSize(300, 300);
+        frame.setSize(1024, 768);
         frame.add(canvas);
         frame.setVisible(true);
         
@@ -41,17 +43,18 @@ public class SimpleScene implements GLEventListener {
     }
     
     public SimpleScene() {
-    	myObjects = new Vector<MyObject>();
-    	//myObjects.add(new Square());
-    	//myObjects.add(new SquareBasedPyramid(-0.5f,-0.5f,10));
-    	myObjects.add(new Star(-0.5f, -0.5f, 1.5f));
+    	
 	}
     
     abstract class MyObject {
-    	public abstract void render(GLAutoDrawable drawable);
+    	public abstract void render(GLAutoDrawable drawable, float z);
     	protected float generateRandom(float min, float max) {
     		Random r = new Random();
     		return r.nextFloat() * (max - min) + min;
+    	}
+    	protected int generateRandom(int min, int max) {
+    		Random r = new Random();
+    		return r.nextInt(max-min)+min;
     	}
     }
     
@@ -73,17 +76,23 @@ public class SimpleScene implements GLEventListener {
     		System.out.println("Square: "+x+", "+y+", "+d);
 		}
     	
-    	public void render(GLAutoDrawable drawable) {
+    	public void render(GLAutoDrawable drawable, float z) {
     		GL2 gl = drawable.getGL().getGL2();
     	    
-    		//gl.glBegin(GL2.GL_QUADS);
-    		//gl.glVertex3f(x, y, 0.0f);
-    		//gl.glVertex3f(x, y+d, 0.0f);
-    		//gl.glVertex3f(x+d, y, 0.0f);
-    		//gl.glVertex3f(x+d, y+d, 0.0f);
-    		gl.glColor3f(0.0f, .0f, 1.0f); 
-    		gl.glRectf(x, y, x+d, y+d);
-    		//gl.glEnd();
+    		gl.glPushMatrix();
+
+    		gl.glBegin(GL2.GL_QUADS);
+    		gl.glPushAttrib(GL2.GL_CURRENT_BIT);
+    		gl.glColor3f(0.0f, 0.0f, 1.0f);
+    		gl.glPopAttrib();
+    		
+    		gl.glVertex3f(x, y, z);
+    		gl.glVertex3f(x, y-d, z);
+    		gl.glVertex3f(x+d, y-d, z);
+    		gl.glVertex3f(x+d, y, z);
+    		
+    		gl.glPopMatrix();
+    		gl.glEnd();
     	}
     }
     
@@ -98,24 +107,36 @@ public class SimpleScene implements GLEventListener {
     		this.levels = levels;
     		squareDiameter = 0.05f;
 		}
+    	public SquareBasedPyramid() {
+    		x = generateRandom(-0.8f, 0.0f);
+    		y = generateRandom(-0.8f, 0.0f);
+    		levels = generateRandom(2, 20);
+    		squareDiameter = generateRandom(0.01f, 0.06f);
+		}
     	
-		public void render(GLAutoDrawable drawable) {
+		public void render(GLAutoDrawable drawable, float z) {
 			float d = squareDiameter;
     		GL2 gl = drawable.getGL().getGL2();
     		gl.glPushMatrix();
     		gl.glBegin(GL2.GL_LINE_LOOP);
-    		gl.glVertex3f(x, y, 0.0f); // start
+    		
+
+    		gl.glPushAttrib(GL2.GL_CURRENT_BIT);
+    		gl.glColor3f(1.0f, 0.0f, 1.0f);
+    		gl.glPopAttrib();
+    		
+    		gl.glVertex3f(x, y, z); // start
     		for(int i=1; i<=levels; i++) {
-        		gl.glVertex3f(x+d*(i-1), y+d*i, 0.0f); // up
-        		gl.glVertex3f(x+d*i, y+d*i, 0.0f); // right
+        		gl.glVertex3f(x+d*(i-1), y+d*i, z); // up
+        		gl.glVertex3f(x+d*i, y+d*i, z); // right
     		}
     		int j=levels;
     		for(int i=levels; i>0; i--) {
-    			gl.glVertex3f(x+d*(j+1), y+d*i, 0.0f); // right
-        		gl.glVertex3f(x+d*(j+1), y+d*(i-1), 0.0f); // down
+    			gl.glVertex3f(x+d*(j+1), y+d*i, z); // right
+        		gl.glVertex3f(x+d*(j+1), y+d*(i-1), z); // down
         		j++;
     		}
-    		gl.glVertex3f(x+d*(levels*2-1), y, 0.0f); // end
+    		gl.glVertex3f(x+d*(levels*2-1), y, z); // end
     		gl.glPopMatrix();
     		gl.glEnd();
 		}
@@ -127,16 +148,57 @@ public class SimpleScene implements GLEventListener {
     	private float y;
     	private float r;
     	public Star(float x, float y, float r) {
+    		if(r == 0.0f){
+        		x = generateRandom(-0.5f, 0.5f);
+        		y = generateRandom(-0.5f, 0.5f);
+        		r = generateRandom(0.05f, 0.4f);
+    		}
     		this.x = x;
     		this.y = y;
     		this.r = r;
 		}
+    	public Star() {
+    		this.x = generateRandom(-0.5f, 0.5f);
+    		this.y = generateRandom(-0.5f, 0.5f);
+    		this.r = generateRandom(0.1f, 0.5f);
+		}
     	
-		public void render(GLAutoDrawable drawable) {
+		public void render(GLAutoDrawable drawable, float z) {
+			GL2 gl = drawable.getGL().getGL2();
+    		
+    		
+    		gl.glPushMatrix();
+    		gl.glBegin(GL2.GL_TRIANGLES);
+    		
+    		gl.glPushAttrib(GL2.GL_CURRENT_BIT);
+    		gl.glColor3f(0.0f, 1.0f, z);
+    		gl.glPopAttrib();
+    		
+    		gl.glVertex3f(x, y+r*1.0f, z);
+    		gl.glVertex3f(x-r*0.8f, y-r*0.5f, z);
+    		gl.glVertex3f(x+r*0.8f, y-r*0.5f, z);
+    		gl.glEnd();
+    		
+    		
+    		gl.glPushMatrix();
+    		gl.glBegin(GL2.GL_TRIANGLES);
+    		
+    		gl.glPushAttrib(GL2.GL_CURRENT_BIT);
+    		gl.glColor3f(0.0f, 1.0f, z);
+    		gl.glPopAttrib();
+    		
+    		gl.glVertex3f(x, y-r*1.0f, z);
+    		gl.glVertex3f(x+r*0.8f, y+r*0.5f, z);
+    		gl.glVertex3f(x-r*0.8f, y+r*0.5f, z);
+    		gl.glEnd();
+    		
+    		gl.glPopMatrix();
+		}
+		public void renderPolygon(GLAutoDrawable drawable, float z) {
     		GL2 gl = drawable.getGL().getGL2();
     		
     		gl.glPushMatrix();
-    		gl.glBegin(GL2.GL_POLYGON);
+    		gl.glBegin(GL2.GL_POLYGON); 
     		/*
     		    .9
     		   .10 .8
@@ -153,23 +215,135 @@ public class SimpleScene implements GLEventListener {
     		x=0.0f;
     		y=0.0f;
     		
-    		gl.glVertex3f(x-r, y, 0.0f);
-    		gl.glVertex3f(x-r*0.3f, y-r*0.2f, 0.0f);
-    		gl.glVertex3f(x-r*0.6f, y-r*0.8f, 0.0f);
-    		gl.glVertex3f(x, y-r*0.2f, 0.0f);
-    		gl.glVertex3f(x+r*0.6f, y-r*0.8f, 0.0f);
-    		gl.glVertex3f(x+r*0.3f, y-r*0.2f, 0.0f);
-    		gl.glVertex3f(x+r, y, 0.0f);
-    		gl.glVertex3f(x+r*0.2f, y+r*0.3f, 0.0f);
-    		gl.glVertex3f(x, y+r*1.0f, 0.0f);
-    		gl.glVertex3f(x-r*0.2f, y+r*0.3f, 0.0f);
+    		gl.glVertex3f(x-r, y, z);
+    		gl.glVertex3f(x-r*0.3f, y-r*0.2f, z);
+    		gl.glVertex3f(x-r*0.6f, y-r*0.8f, z);
+    		gl.glVertex3f(x, y-r*0.2f, z);
+    		gl.glVertex3f(x+r*0.6f, y-r*0.8f, z);
+    		gl.glVertex3f(x+r*0.3f, y-r*0.2f, z);
+    		gl.glVertex3f(x+r, y, z);
+    		gl.glVertex3f(x+r*0.2f, y+r*0.3f, z);
+    		gl.glVertex3f(x, y+r*1.0f, z);
+    		gl.glVertex3f(x-r*0.2f, y+r*0.3f, z);
     		gl.glEnd();
     		gl.glPopMatrix();
-    		//System.out.println("rendering star...");
+    		//System.out.println(x+","+y+","+r);
 		}
     	
     }
 
+    /**
+     * Have more than one viewport where you draw the scene from
+     *  a different point of view.
+     * Demonstrate some more advanced OpenGL 
+     *  implementation (vertex arrays, display lists)
+     * Buffer!
+     * @author oskkla-9
+     *
+     */
+    class MoreComplexPrimitive extends MyObject {
+    	private int indiciesArray[] = {
+			0,1,2,3,4,5,6,7
+    	}; 
+		final int VERTICES=8, VELEMENTS=3, 
+				BUFFER_SIZE=VERTICES*VELEMENTS*8;
+		ByteBuffer vertices = ByteBuffer.allocateDirect(BUFFER_SIZE);
+		ByteBuffer indicies = ByteBuffer.allocateDirect(VERTICES*8);
+		ByteBuffer colors = ByteBuffer.allocateDirect(BUFFER_SIZE);
+    	
+    	public MoreComplexPrimitive() {
+    		float vertexArray[][] = {
+    			{0.0f,0.0f,1.0f},
+    			{0.0f,0.0f,1.0f},
+    			{0.0f,0.0f,1.0f},
+    			{0.0f,0.0f,1.0f},
+    			{0.0f,0.0f,1.0f},
+    			{0.0f,0.0f,1.0f},
+    			{0.0f,0.0f,1.0f},
+    			{0.0f,0.0f,1.0f}
+    		};
+    		float colorsArray[][] = {
+    			{0.0f,1.0f,1.0f},
+    			{0.0f,1.0f,1.0f},
+    			{0.0f,1.0f,1.0f},
+    			{0.0f,1.0f,1.0f},
+    			{0.0f,1.0f,1.0f},
+    			{0.0f,1.0f,1.0f},
+    			{0.0f,1.0f,1.0f},
+    			{0.0f,1.0f,1.0f}
+    		};
+    		int indiciesArray[] = {
+				0,1,2,3,4,5,6,7
+	    	}; 
+    		vertices = getBufferFromArrays(vertexArray);
+    		colors = getBufferFromArrays(colorsArray);
+    		indicies = getBufferFromArrays(indiciesArray);
+		}
+
+    	private ByteBuffer getBufferFromArrays(float vertexArray[][]) {
+    		int i,j;
+    		ByteBuffer ret = ByteBuffer.allocateDirect(BUFFER_SIZE);
+    		for(i=0;i<VERTICES;i++){
+    			for(j=0; j<VELEMENTS; j++) {
+    				ret.putFloat(vertexArray[i][j]);
+	    		}
+    		}
+    		ret.rewind();
+    		return ret;
+    	}
+    	private ByteBuffer getBufferFromArrays(int indiciesArray[]) {
+    		int i;
+    		ByteBuffer ret = ByteBuffer.allocateDirect(VERTICES*8);
+    		for(i=0;i<VERTICES;i++){
+				ret.putFloat(indiciesArray[i]);
+    		}
+    		ret.rewind();
+    		return ret;
+    	}
+    	
+    	
+    	public void render(GLAutoDrawable drawable, float z) {
+    		GL2 gl = drawable.getGL().getGL2();
+    		//gl.glViewport(arg0, arg1, arg2, arg3);
+    		//gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+    		gl.glMatrixMode(GL2.GL_MODELVIEW);
+    		gl.glLoadIdentity();
+    		
+    		float vertexArray[][] = {
+    			{0.0f,0.0f,0.5f},
+    			{0.0f,-0.5f,0.5f},
+    			{0.5f,-0.5f,0.5f},
+    			{0.5f,0.5f,0.5f},
+    			{0.5f,0.5f,0.5f},
+    			{0.5f,0.1f,0.5f},
+    			{0.1f,0.1f,0.5f},
+    			{0.1f,0.5f,0.5f}
+    		};
+    		float colorsArray[][] = {
+    			{0.0f,100.0f,100.0f},
+    			{0.0f,100.0f,100.0f},
+    			{0.0f,100.0f,100.0f},
+    			{0.0f,100.0f,100.0f},
+    			{0.0f,100.0f,100.0f},
+    			{0.0f,100.0f,100.0f},
+    			{0.0f,100.0f,100.0f},
+    			{0.0f,100.0f,100.0f}
+    		};
+    		vertices = getBufferFromArrays(vertexArray);
+    		colors = getBufferFromArrays(colorsArray);
+    		//System.out.println("derp");
+    		// glXPointer(size, type, stride, pointer):
+    		
+    	    gl.glVertexPointer(3, GL2.GL_FLOAT, 0, vertices);
+    		gl.glColorPointer(3,GL2.GL_FLOAT, 0, colors); 
+    		gl.glDrawArrays(GL.GL_TRIANGLES, 0, 8);
+    		//gl.glDrawElements(GL2.GL_TRIANGLES,1,GL.GL_UNSIGNED_INT, indicies);
+    		
+    		gl.glFlush();
+    	}
+    }
+    
 	@Override
 	public void display(GLAutoDrawable drawable) {
 		update();
@@ -183,9 +357,27 @@ public class SimpleScene implements GLEventListener {
 	}
 
 	@Override
-	public void init(GLAutoDrawable arg0) {
-		// TODO Auto-generated method stub
-		
+	public void init(GLAutoDrawable drawable) {
+		// https://docs.google.com/file/d/0B9hhZie2D-fEZGI2NTZhZTMtYWYwNS00NTljLWFiNGQtM2UyNTYyNjAzNDYy/edit?hl=en
+
+    	myObjects = new Vector<MyObject>();
+    	myObjects.add(new Square());
+    	myObjects.add(new SquareBasedPyramid());
+    	myObjects.add(new Star());
+    	myObjects.add(new MoreComplexPrimitive());
+    	
+    	
+		GL2 gl = drawable.getGL().getGL2();
+		if(!gl.isExtensionAvailable("GL_ARB_vertex_buffer_object")){
+			 System.out.println("Error: VBO support is missing");
+		}
+		gl.glMatrixMode(GL2.GL_PROJECTION); 
+		gl.glLoadIdentity();
+		gl.glEnable(GL.GL_DEPTH_TEST);
+		gl.glEnableClientState(GL2.GL_COLOR_ARRAY);
+		gl.glEnableClientState(GL2.GL_VERTEX_ARRAY);
+		gl.glClearColor(0f, 0f, 0f ,0f);
+		gl.glColor3f(1f, 1f, 1f);
 	}
 
 	@Override
@@ -197,12 +389,16 @@ public class SimpleScene implements GLEventListener {
 	
 	private void render(GLAutoDrawable drawable) {
 	    GL2 gl = drawable.getGL().getGL2();
+		gl.glMatrixMode(GL2.GL_MODELVIEW); 
+		gl.glLoadIdentity();
 	    
 	    // black
-        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
 	    
+        float z = 0.0f;
         for (MyObject o : myObjects){
-        	o.render(drawable);
+        	o.render(drawable, z);
+        	z = z+0.1f;
         }
 	}
 	
